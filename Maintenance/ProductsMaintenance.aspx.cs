05d4be95b1cbbdcd4b6b47235951d65fc74a146a
Justin Lugo <jlug331221@gmail.com>
2016-04-22 11:51:25 -0400

    Working on maintenance page for products.

---
+++ Maintenance/ProductsMaintenance.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Maintenance_ProductsMaintenance : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+
+    protected void DetailsView1_ItemUpdated(
+        object sender, DetailsViewUpdatedEventArgs e)
+    {
+        if (e.Exception != null)
+        {
+            lblError.Text = "A database error has occurred. " +
+                "Message: " + e.Exception.Message;
+            e.ExceptionHandled = true;
+            e.KeepInEditMode = true;
+        }
+        else if (e.AffectedRows == 0)
+            lblError.Text = "Another user may have updated that product. " +
+                "Please try again.";
+        else
+            GridView1.DataBind();
+    }
+
+    protected void DetailsView1_ItemDeleted(
+        object sender, DetailsViewDeletedEventArgs e)
+    {
+        if (e.Exception != null)
+        {
+            lblError.Text = "A database error has occurred. " +
+                "Message: " + e.Exception.Message;
+            e.ExceptionHandled = true;
+        }
+        else if (e.AffectedRows == 0)
+            lblError.Text = "Another user may have updated that product. " +
+                "Please try again.";
+        else
+            GridView1.DataBind();
+    }
+
+    protected void DetailsView1_ItemInserted(
+        object sender, DetailsViewInsertedEventArgs e)
+    {
+        if (e.Exception != null)
+        {
+            lblError.Text = "A database error has occurred. " +
+                "Message: " + e.Exception.Message;
+            e.ExceptionHandled = true;
+            e.KeepInInsertMode = true;
+        }
+        else
+            GridView1.DataBind();
+    }
+
+    protected void DetailsView1_ItemDeleting(
+        object sender, DetailsViewDeleteEventArgs e)
+    {
+        e.Values["UnitPrice"]
+            = e.Values["UnitPrice"].ToString().Substring(1);
+    }
+}
